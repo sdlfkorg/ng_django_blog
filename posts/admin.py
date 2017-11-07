a3caf25b2c36cb2989b7d043d7b3e42e67aedb65
sdlfkorg <sdlfkorg@gmail.com>
2017-11-07 20:44:40 +0800

    add Post model

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from posts.models import Post
 
 # Register your models here.
+
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ['title', 'user_name', 'timestamp']
+    class Meta:
+        model = Post
+
+admin.site.register(Post)
