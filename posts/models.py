9b7298fceabc7d17e5ce593524197fc4d72ec5df
sdlfkorg <sdlfkorg@gmail.com>
2017-11-07 21:07:01 +0800

    delete slug field temporary

--- posts/models.py
+++ posts/models.py
@@ -6,7 +6,7 @@ from django.conf import settings
 class Post(models.Model):
     user_name = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
     title = models.CharField(max_length=120)
-    slug = models.SlugField(unique=True)
+    # slug = models.SlugField(unique=True)
     content = models.TextField()
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
