e19615f0e42e7489af68a5848072144393942ee1
sdlfkorg <sdlfkorg@gmail.com>
2017-11-13 20:21:30 +0800

    test the get feature of Post from django

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,35 @@
-app = angular.module('demoapp', []);
+app = angular.module('app', [])
 
-app.controller('DemoCtrl', ['$scope', function($scope){
-	$scope.num = 0;
-	$scope.save = function(){
-		$(".data").html("Click: " + $scope.num);
-		$scope.num += 1;
-	}
+app.controller('DemoCtrl', ['$scope', '$http', function($scope, $http){
+    //  $scope.num = 0;
+    //  $scope.save = function(){
+    //      $(".data").html("Click: " + $scope.num);
+    //      $scope.num += 1;
+    //  };
+
+    // $scope.posts = [{
+    //     user_name: 'Joe',
+    //     title: 'Sample Post #1',
+    //     content: 'This is the first sample post'
+    //     }
+    // 	,{
+    //     user_name: 'Karen',
+    //     title: 'Sample Post #2',
+    //     content: 'This is another sample post'
+	   //  }
+    // ];
+    // console.log($scope.posts);
+    var data = $http.get('/api/post').then(
+    	function(response){
+    		console.log('response');
+    		console.log(response);
+    		$scope.posts = response.data;
+    	},
+    	function(errorResponse){
+    		console.log(errorResponse);
+    	}
+    );
+    // console.log($scope.data);
+
 
 }]);
