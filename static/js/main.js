2828db0fcd850b60cb5f0ba010aa4b8f76e6af50
jay.liao <jay.liao@tw.promise.com>
2018-01-25 14:33:22 +0800

    import post service into template and main.js

--- static/js/main.js
+++ static/js/main.js
@@ -3,7 +3,7 @@
   'use strict';
   
   angular
-    .module('app', ['ngResource', 'ngRoute'])
+    .module('app', ['ngResource', 'ngRoute', 'app.post.service'])
     .config(RouterConfig)
     .controller('DemoCtrl', DemoCtrl);
 
@@ -11,36 +11,42 @@
   RouterConfig.$inject = ['$routeProvider', '$locationProvider'];
   
   function RouterConfig($routeProvider, $locationProvider) {
-    //to allow vskyconsole protocol
     $routeProvider.when(
-            '/',
-            {
-                templateUrl : '/static/js/templates/index.html',
-                controller  : 'DemoCtrl'
-            }
-        ).when(
-            '/about', 
-            {
-                templateUrl : '/static/js/templates/about.html',
-            }
-        )
-    
+      '/',
+        {
+            templateUrl : '/static/js/templates/index.html',
+            controller  : 'DemoCtrl'
+        }
+      ).when(
+        '/about', 
+        {
+            templateUrl : '/static/js/templates/about.html',
+        }
+      );
   }
 
-  DemoCtrl.$inject = ['$scope', '$http'];
+  DemoCtrl.$inject = ['$scope', '$http', 'PostService'];
 
-  function DemoCtrl($scope, $http){
+  function DemoCtrl($scope, $http, Post){
 
-    var data = $http.get('/api/post').then(
-        function(response){
-            console.log('response');
-            console.log(response);
-            $scope.posts = response.data;
-        },
-        function(errorResponse){
-            console.log(errorResponse);
-        }
+    var data = Post.query().$promise.then(
+      function(response){
+        console.log('response from resource');
+        console.log(response);
+        $scope.posts = response;
+      },
+      function(errResponse){
+        console.log(errResponse);
+      }
     );
+
+    // var data = $http.get('/api/post').then(
+    //   function(response){
+    //     console.log('response');
+    //     console.log(response);
+    //     $scope.posts = response.data;
+    //   }
+    // );
 
   }
   
